7ae39392c74ab318055ba3bbc72a04b7d3791aa2
Pavithra Chidambaram Pappa <pavithra.chidambaram@consensys.net>
2020-04-28 14:26:47 -0700

    Import resolver

--- contracts/token/ERC20/KaleidoERC20.sol
+++ contracts/token/ERC20/KaleidoERC20.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../../node_modules/openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KaleidoERC20 is ERC20 {
     constructor(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) ERC20(name, symbol) public {
